15522bea1d691b4c6fc7f6279098728f4f99febd
Luiz Belasco <luizcarlos.belasco@gmail.com>
2021-07-07 20:28:35 -0300

    adicionando regras

--- Menu.java
+++ Menu.java
@@ -1,8 +1,11 @@
-
-public class Menu {
-
+public class Menu{
 	public static void main(String[] args) {
-		System.out.println("Bem vindo a sua calculadora pessoal!");
+		System.out.println("Bem Vindo a sua Calculadora Pessoal!");
+		int constante, variavel;
+		System.out.println("Como usar:");
+		System.out.println("1-digite seu primeiro valor");
+		System.out.println("2-digite sua operação");
+		System.out.println("3-repita quantas vezes quiser");
+		System.out.println("4-quando quiser parar digite sinal de igual(=)");
 	}
-
 }
