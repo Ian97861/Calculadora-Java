900d0e7e203548e8e9bd191d67109ee0f50598fe
João <joaopedrosantos.joca@gmail.com>
2021-07-06 21:18:03 -0300

    teste

--- Menu.java
+++ Menu.java
@@ -2,7 +2,7 @@
 public class Menu {
 
 	public static void main(String[] args) {
-		System.out.println("Bem vindo a sua calculadora pessoal.");
+		System.out.println("Bem vindo a sua calculadora pessoal!");
 	}
 
 }
