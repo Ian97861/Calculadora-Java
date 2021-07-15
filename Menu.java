54486da2ad630f45caf8745ad1f0e905680bb5c7
Ian Menezes Carvalho <iancarvalho33@yahoo.com.br>
2021-07-14 22:11:48 -0300

    Adicionando operações trigonométricas

--- Menu.java
+++ Menu.java
@@ -8,7 +8,7 @@ public class Menu{
         Scanner scan = new Scanner(System.in);
         System.out.println("Como usar:");
         System.out.println("1-Digite seu primeiro valor");
-        System.out.println("2-Digite sua operação");
+        System.out.println("2-Digite sua operacao");
         System.out.println("3-Repita quantas vezes quiser");
         System.out.println("4-Quando quiser parar digite sinal de igual(=)");
         constante= scan.nextInt();
@@ -32,16 +32,24 @@ public class Menu{
                 	constante *= constante;
                 } else if (operacao.charAt(0)== 'v' ) {
                 	constante = Math.sqrt(constante);
-                }
-                	
-                  else {
-                	System.out.println("Símbolo de operação inválido, digite novamente o sinal");
-                }
-
-            
+                } else if(operacao.charAt(0)== 'c' && operacao.charAt(1)== 'o' && operacao.charAt(2)== 's') {
+                	constante = Math.cos(constante);
+                } else if(operacao.charAt(0)== 's' && operacao.charAt(1)== 'e' && operacao.charAt(2)== 'n') {
+                	constante = Math.sin(constante);
+                } else if(operacao.charAt(0)== 't' && operacao.charAt(1)== 'a' && operacao.charAt(2)== 'n') {
+                	constante = Math.tan(constante);
+                } else if(operacao.charAt(0)== 'l' && operacao.charAt(1)== 'o' && operacao.charAt(2)== 'g') {
+                	constante = Math.log(constante);
+                } else if(operacao.charAt(0)== 'm' && operacao.charAt(1)== 'o' && operacao.charAt(2)== 'd') {
+                	constante = Math.abs(constante); 
+                }	
+                		else 
+                	{			
+                	System.out.println("Simbolo de operacao invalido, digite novamente o sinal");
             }	            
             System.out.println(constante);
            
         }
     }
+}
 }
